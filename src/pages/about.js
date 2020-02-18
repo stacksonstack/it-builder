72130577ae2f6fc02e0ab38a96a2490d1394cab6
stacey carrillo <stacey@Staceys-MacBook-Air.local>
2020-02-18 15:18:19 -0500

    stacey commiting about page creation, still needs to link

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi from stacey and anthony</h1>
+    <h2>Firsts of firsts</h2>
+    <p>Stacey's first gatsby project and first shared repo and first gatsby page.......🤓 </p>
+  </Layout>
+)
+
+export default AboutPage
